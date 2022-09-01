8a95bde05aa9960381383dba1f1fce67636ac607
hansontram <htram.dev@gmail.com>
2022-09-01 11:17:40 -0700

    multiply word in string

---
+++ 08-31-2022-cc main.js
@@ -0,0 +1,90 @@
+// Add property to every object in array
+// Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+
+// For example:
+
+// var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+//     corAnswer: 0
+// }];
+// After adding the property the result should be:
+
+// var questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }];
+// The questions array is already defined for you and is not the same as the one in the example.
+
+//for each method 
+function addProperty (questions){
+  questions.forEach ((element) => {
+    element.userAnswer= null
+    questions.push(element)
+  })
+  
+}
+console.log(questions)
+
+//map method
+function addProperty (questions){
+  const object = questions.map((element) => ({
+    ...element ,
+    usersAnswer: null
+  }));
+console.log(object)
+}
+
+//for loop method
+function addProperty (questions){
+    for (let i =0; i<questions.length;i++){
+      questions[i].usersAnswer = null
+    }
+    console.log(questions)
+  }
+  
+  
+  // test
+  var myQuestions = [{
+      question: "What's the currency of the USA?",
+      choices: ["US dollar", "Ruble", "Horses", "Gold"],
+      corAnswer: 0
+  }, {
+      question: "Where was the American Declaration of Independence signed?",
+      choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+      corAnswer: 0
+  }];
+  
+  addProperty(myQuestions);
+
+
+//   Sort array by string length
+//   Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+
+//   For example, if this array were passed as an argument:
+  
+//   ["Telescopes", "Glasses", "Eyes", "Monocles"]
+  
+//   Your function would return the following array:
+  
+//   ["Eyes", "Glasses", "Monocles", "Telescopes"]
+  
+//   All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+
+function sortByLength (array) {
+    array.sort((a, b) => a.length - b.length);
+     return array
+   };
+
+   /////
--- 09-01-2022-cc main.js
+++ 09-01-2022-cc main.js
@@ -1,88 +1,72 @@
-// Add property to every object in array
-// Your task is to add a new property usersAnswer to every object in the array questions. The value of usersAnswer should be set to null. The solution should work for array of any length.
+// Capitalize first letter of a string
+// Write a function capitalize() which capitalizes the first letter (if any) of the given string. For example:
 
-// For example:
+// Input	Output
+// string	String
+// hello World	Hello World
+// i love codewars	I love codewars
+// This sentence is already capitalized	This sentence is already capitalized
+// 0123the first character of this sentence is not a letter	0123the first character of this sentence is not a letter
+// JavaScript / CoffeeScript: Extend the String prototype with a method capitalize() so you can call it on a string like so: "string".capitalize(). Learn about inheritance and the prototype chain.
 
-// var questions = [{
-//     question: "What's the currency of the USA?",
-//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
-//     corAnswer: 0
-// }, {
-//     question: "Where was the American Declaration of Independence signed?",
-//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
-//     corAnswer: 0
-// }];
-// After adding the property the result should be:
+// Furthermore, the built-in string methods toUpperCase() and toLowerCase() are disabled for this Kata.
 
-// var questions = [{
-//     question: "What's the currency of the USA?",
-//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
-//     corAnswer: 0,
-//     usersAnswer: null
-// }, {
-//     question: "Where was the American Declaration of Independence signed?",
-//     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
-//     corAnswer: 0,
-//     usersAnswer: null
-// }];
-// The questions array is already defined for you and is not the same as the one in the example.
+//PREP
+//Paramaters: String 
+//Return: return
+//Example: 'string' --> 'String', 'hello world' --> 'Hello World'
+//Pseudo:
 
-//for each method 
-function addProperty (questions){
-  questions.forEach ((element) => {
-    element.userAnswer= null
-    questions.push(element)
-  })
-  
-}
-console.log(questions)
-
-//map method
-function addProperty (questions){
-  const object = questions.map((element) => ({
-    ...element ,
-    usersAnswer: null
-  }));
-console.log(object)
+function capitalize(str){
+    // use charAt(0) to isolate the first letter
+    //toUpperCase to capitalize
+    //slice to giove start and end position 
+    str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-//for loop method
-function addProperty (questions){
-    for (let i =0; i<questions.length;i++){
-      questions[i].usersAnswer = null
-    }
-    console.log(questions)
+String.prototype.capitalize = function () {
+    let c = this.charCodeAt(0)
+    if (97 <= c && c <= 122) c -= 32
+    
+    return String.fromCharCode(c) + this.slice(1)
   }
-  
-  
-  // test
-  var myQuestions = [{
-      question: "What's the currency of the USA?",
-      choices: ["US dollar", "Ruble", "Horses", "Gold"],
-      corAnswer: 0
-  }, {
-      question: "Where was the American Declaration of Independence signed?",
-      choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
-      corAnswer: 0
-  }];
-  
-  addProperty(myQuestions);
 
+//   You are to write an function that takes a string as it's first paramter. This string will be a string of words.
 
-//   Sort array by string length
-//   Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+// You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
 
-//   For example, if this array were passed as an argument:
-  
-//   ["Telescopes", "Glasses", "Eyes", "Monocles"]
-  
-//   Your function would return the following array:
-  
-//   ["Eyes", "Glasses", "Monocles", "Telescopes"]
-  
-//   All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+// Once you have the located string you are finally going to multiply by it the third provided paramater, which will also be an interger. You are additionally required to add a hyphen in between each word.
 
-function sortByLength (array) {
-    array.sort((a, b) => a.length - b.length);
-     return array
-   };
+// Example
+
+// modifyMultiply ("This is a string",3,5) 
+// Should return
+
+// "string-string-string-string-string"
+
+// Since the 3rd word is 'string'(starting from 0 remember) and the third paramater indicates that it should be repeated 5 times.
+
+// Simple. Good luck.
+
+//Paramator: 1-string, 2-integer(locate), 3-integer(mulitply)
+//Return: return string
+//Example: ("This is a string",3 ,5) --> "string-string-string-string-string"
+//Pseudo:
+
+function modifyMultiply (str,loc,num) {
+    // use .split to split string of words into array 
+    //use [loc] to use 2nd param to locate string
+    // run for loop (num) times 
+    // conditional for hyphen at the end 
+   let word= str.split(" ")[loc]
+     let result = ""
+   for (let i=0; i<num; i++){
+     if (i === num-1){
+       result += word
+     } else {
+       result += word+"-"
+     }
+     
+  }
+    return (result)
+  } 
