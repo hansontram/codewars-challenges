210f31da2a5bfe92f6af27490f0ec4a427b15ceb
hansontram <htram.dev@gmail.com>
2022-09-01 11:41:46 -0700

    adding daily challenges

--- 08-14-2022-cc main.js
+++ 08-14-2022-cc main.js
@@ -24,3 +24,14 @@ function gooseFilter (birds) {
 // 3. includes determins whether an array includes a certain value 
 //   3.a) returns a true or false (we want the false values to be returned) 
 // 4. Non-Geese elements are returned
+
+// You are to write an function that takes a string as it's first paramter. This string will be a string of words.
+
+// You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
+
+// Once you have the located string you are finally going to multiply by it the third provided paramater, which will also be an interger. You are additionally required to add a hyphen in between each word.
+
+// Example
+function modifyMultiply(str,i,n) {
+  return Array(n).fill(str.split(' ')[i]).join('-');
+} 
