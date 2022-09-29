228b7576daf271b2ef546bf99c726fb5121491a4
hansontram <htram.dev@gmail.com>
2022-09-29 11:25:28 -0700

    Factorials

---
+++ 09-29-2022-cc main.js
@@ -0,0 +1,38 @@
+Factorial
+
+Your task is to write function factorial.
+
+https://en.wikipedia.org/wiki/Factorial
+
+
+
+//P:number
+//R:number
+  
+  //declare variable with 1
+  //for loop, iterate through number
+  //reassign variable to multiply with element
+  //return element 
+
+
+//my solution 
+function factorial(n){
+    let result = 1
+    for (let i=1; i<=n;i++){
+      result = i * n
+    }
+    return multiply
+  }
+  
+
+
+  
+  //solution 1
+  function factorial(n){
+    let sum = 1;
+     for (let i = 1; i <= n; i++){
+       sum = sum * i;
+     }
+     return sum; 
+   
+   }
