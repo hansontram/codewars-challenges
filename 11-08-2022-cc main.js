996d0275488ac7b509ae409b87b8d3527a8dcccb
hansontram <htram.dev@gmail.com>
2022-11-08 14:03:37 -0700

    Testing 1-2-3

---
+++ 11-08-2022-cc main.js
@@ -0,0 +1,18 @@
+Testing 1-2-3
+
+var number=function(array){
+    let arr = []
+    for (let i=0;i<array.length;i++){
+      arr.push(`${i +1}` + `: ${array[i]}`)
+    }
+    return arr
+  }
+  
+  // an array of letters
+  // array 1:a
+  
+  //declare var with empty array
+  //for loop
+  //template literals 
+  //push to arr
+  
