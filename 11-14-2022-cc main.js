65cb8414c806669823eef17a1e84f46371ca3ee1
hansontram <htram.dev@gmail.com>
2022-11-14 12:53:53 -0700

    Find the remainder

---
+++ 11-14-2022-cc main.js
@@ -0,0 +1,58 @@
+Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+Division by zero should return NaN.
+
+Examples:
+n = 17
+m = 5
+result = 2 (remainder of `17 / 5`)
+
+n = 13
+m = 72
+result = 7 (remainder of `72 / 13`)
+
+n = 0
+m = -1
+result = 0 (remainder of `0 / -1`)
+
+n = 0
+m = 1
+result - division by zero (refer to the specifications on how to handle this in your language)
+
+
+
+
+
+
+
+
+function remainder(n, m){
+    if (n>m){
+      return n % m
+    } else if (m>=n){
+      return m%n
+    }else if (n==0 || m==0){
+       return NaN
+    }
+    
+  }
+
+
+  const remainder = (a, b) => Math.max(a,b) % Math.min(a,b);
+  
+
+
+  function remainder(a, b){
+    return a < b ? b%a :a%b;  
+  }
+  
+  // number n and m
+  // number, remainder 
+  // n=17, m=5, --> 2
+  
+  // determine if n or m is larger
+  // coditional 
+  // if n >m : n % m
+  // if m> n : m % n
+  // return 
+  
