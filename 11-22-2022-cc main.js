4dd8dff1d09d3b8b47b3af5802f4dafb4b64fc9f
hansontram <htram.dev@gmail.com>
2022-11-23 22:44:22 -0800

    Power

---
+++ 11-22-2022-cc main.js
@@ -0,0 +1,19 @@
+Alan is known for referring to the temperature of the apple turnover as Hotter than the sun!. According to space.com the temperature of the sun's corona is 2,000,000 degrees Celsius, but we will ignore the science for now.
+
+Task
+Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+
+
+function apple(x){
+    if (Math.pow(x,2) > 1000){
+      return "It's hotter than the sun!!"
+    } else {
+      return "Help yourself to a honeycomb Yorkie for the glovebox."
+    }
+    }
+
+
+    const apple = (x) => { return Math.pow(x,2)>1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."}
+
+
