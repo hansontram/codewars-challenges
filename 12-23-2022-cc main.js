ab7bbed3ed2d2a2299c0b3c5417d07abbc99d6db
hansontram <htram.dev@gmail.com>
2022-12-23 12:13:12 -0800

    Find the most digits

---
+++ 12-23-2022-cc main.js
@@ -0,0 +1,29 @@
+Find the number with the most digits.
+
+If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+
+
+function findLongest(array){
+    let num = '0';
+    for(let i=0; i<array.length; i++) {
+      let string = array[i].toString();
+      if(string.length > num.length) {
+        num = string;
+      }
+    }
+    return parseInt(num);
+  }
+
+
+  
+
+  function findLongest(array){
+    var newArr = array.map(function(element) {
+      return element.toString()
+    })
+    newArr.sort(function(a,b) {
+      return b.length - a.length
+    })
+    return parseInt(newArr[0])
+    }
